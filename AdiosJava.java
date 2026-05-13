4201754e1003e3e5017f7c180c6e25d1da0c10ef
gerardofloresk160-web <gerardofloresk106@gmail.com>
2026-05-13 12:45:42 -0600

    Se agrego un programa que suma 2 numeros

--- AdiosJava.java
+++ AdiosJava.java
@@ -1,6 +1,19 @@
 public class AdiosJava {
     public static void main(String[] args) {
-        System.out.println("Adios, Java!");
-        System.out.println("comenzaremos a usar python");
+        //System.out.println("Adios, Java!");
+        //System.out.println("comenzaremos a usar python");
+
+        int numero1, numero2;
+        Scanner scanner = new Scanner();
+        System.out.print("Ingrese el primer número: ");
+        numero1 = scanner.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        numero2 = scanner.nextInt();
+        int suma = numero1 + numero2;
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+
+
+    
+
     }
 }
