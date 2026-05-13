7f58a9abed5b54b8ce94d40eb731efb4fc06fba7
gerardofloresk160-web <gerardofloresk106@gmail.com>
2026-05-13 12:33:19 -0600

    Se agrego una salida de pantalla

--- AdiosJava.java
+++ AdiosJava.java
@@ -1,5 +1,6 @@
 public class AdiosJava {
     public static void main(String[] args) {
         System.out.println("Adios, Java!");
+        System.out.println("comenzaremos a usar python");
     }
 }
