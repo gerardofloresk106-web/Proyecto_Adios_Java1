db0737ae82b1230ed4acb80ba878a905309b8402
gerardofloresk160-web <gerardofloresk106@gmail.com>
2026-05-17 01:21:27 -0600

    Se agrego un metodo que resta dos numeros y la parte del metodo que muestra el resultado de la resta

--- AdiosJava.java
+++ AdiosJava.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class AdiosJava {
     public static void main(String[] args) {
+       
         //System.out.println("Adios, Java!");
         //System.out.println("comenzaremos a usar python");
-
         int numero1, numero2;
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el primer número: ");
         numero1 = scanner.nextInt();
         System.out.print("Ingrese el segundo número: ");
@@ -12,8 +14,11 @@ public class AdiosJava {
         int suma = numero1 + numero2;
         System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
 
-
+        int resta = restar(numero1, numero2);
+        System.out.println("La resta de " + numero1 + " y " + numero2 + " es: " + resta);
     
-
     }
+    public static int restar(int num1, int num2) {
+        return num1 - num2;
+ }
 }
